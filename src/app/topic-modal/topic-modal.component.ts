5504bc9817489e0e26d5356ac4f2db7d20ba4c7c
raharman <r.harbulak3@gmail.com>
2022-01-18 22:39:00 +0100

    pridal som modal komponent pre mat. temy

---
+++ src/app/topic-modal/topic-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-topic-modal',
+  templateUrl: './topic-modal.component.html',
+  styleUrls: ['./topic-modal.component.scss'],
+})
+export class TopicModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
