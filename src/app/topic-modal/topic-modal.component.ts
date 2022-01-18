e29a9928627b162e2979995447e135d3331f5e4a
raharman <r.harbulak3@gmail.com>
2022-01-18 22:41:40 +0100

    upravil som modal komponent

--- src/app/topic-modal/topic-modal.component.ts
+++ src/app/topic-modal/topic-modal.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-topic-modal',
   templateUrl: './topic-modal.component.html',
   styleUrls: ['./topic-modal.component.scss'],
 })
-export class TopicModalComponent implements OnInit {
+export class TopicModalComponent {
 
-  constructor() { }
+  constructor(private modalCtrl: ModalController) { }
 
-  ngOnInit() {}
+  dismissModal() {
+    this.modalCtrl.dismiss();
+  }
 
 }
